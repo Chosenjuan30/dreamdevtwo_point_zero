0ea99e7ffd563ea413b8e55f4e259fdb4c89e3ed
John Agene <johnagene29@gmail.com>
2026-03-21 19:58:26 +0100

    Add src folder

---
+++ src/dsa/RiderPayment.java
@@ -0,0 +1,25 @@
+package dsa;
+
+public class RiderPayment {
+
+    public static int calculateWage(int successfulDeliveries) {
+        int basePay = 5000;
+        int totalPackages = 100;
+
+        double percentage = (successfulDeliveries * 100.0) / totalPackages;
+
+        int rate;
+
+        if (percentage < 50) {
+            rate = 160;
+        } else if (percentage < 60) {
+            rate = 200;
+        } else if (percentage < 70) {
+            rate = 250;
+        } else {
+            rate = 500;
+        }
+
+        return (successfulDeliveries * rate) + basePay;
+    }
+}
---
+++ src/dsa/RiderPaymentTest.java
@@ -0,0 +1,39 @@
+package dsa;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RiderPaymentTest {
+
+    @Test
+    public void testLessThan50Percent() {
+        assertEquals(11400, RiderPayment.calculateWage(40));
+    }
+
+    @Test
+    public void testBetween50And59Percent() {
+        assertEquals(16000, RiderPayment.calculateWage(55));
+    }
+
+    @Test
+    public void testBetween60And69Percent() {
+        assertEquals(21250, RiderPayment.calculateWage(65));
+    }
+
+    @Test
+    public void testAbove70Percent() {
+        assertEquals(45000, RiderPayment.calculateWage(80));
+    }
+
+    @Test
+    public void testZeroDeliveries() {
+        assertEquals(5000, RiderPayment.calculateWage(0));
+    }
+
+    @Test
+    public void testFullDeliveries() {
+        assertEquals(55000, RiderPayment.calculateWage(100));
+    }
+}
